1faefe7642784b2a2c4741463d53868ba0a97c14
grzegorz@musial.tk <grzegorz@musial.tk>
2024-09-15 00:51:04 +0200

    chore: TDD: failing test

--- activity-indoor-cycling/src/main/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoader.java
+++ activity-indoor-cycling/src/main/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoader.java
@@ -1,10 +1,14 @@
 package tk.musial.activityprocessor.indoorcycling;
 
+import java.io.InputStream;
 import java.util.List;
 import tk.musial.activityprocessor.SampleInLap;
 import tk.musial.activityprocessor.SamplesLoader;
 
 public class IndoorCyclingHeartRateSamplesLoader implements SamplesLoader {
+
+  public IndoorCyclingHeartRateSamplesLoader(InputStream inputStream) {}
+
   @Override
   public List<SampleInLap> load() {
     return null;
--- activity-indoor-cycling/src/test/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoaderTest.java
+++ activity-indoor-cycling/src/test/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoaderTest.java
@@ -2,14 +2,21 @@ package tk.musial.activityprocessor.indoorcycling;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import java.io.InputStream;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class IndoorCyclingHeartRateSamplesLoaderTest {
 
-
-    @Test
-    void failsToLoadInvalidJson() {
-
-        new IndoorCyclingHeartRateSamplesLoader(this.getClass().getResourceAsStream("samples.invalid.json"));
-    }
+  @Test
+  void failsToLoadInvalidJson() {
+    Assertions.assertThatExceptionOfType(JsonParseException.class)
+        .isThrownBy(
+            () -> {
+              InputStream jsonResource =
+                  this.getClass().getResourceAsStream("samples.invalid.json");
+              new IndoorCyclingHeartRateSamplesLoader(jsonResource).load();
+            });
+  }
 }

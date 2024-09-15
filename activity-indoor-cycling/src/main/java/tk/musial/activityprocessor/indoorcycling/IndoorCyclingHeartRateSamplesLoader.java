b05e7a13fccfa88899e4ac119e341b0e692d2348
grzegorz@musial.tk <grzegorz@musial.tk>
2024-09-15 23:09:28 +0200

    chore: TDD triangulation: failing test for different case to force the next step towards actual implementation

--- activity-indoor-cycling/src/main/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoader.java
+++ activity-indoor-cycling/src/main/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoader.java
@@ -1,10 +1,8 @@
 package tk.musial.activityprocessor.indoorcycling;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import java.io.InputStream;
 import java.util.List;
-
-import com.fasterxml.jackson.core.JsonParseException;
-
 import tk.musial.activityprocessor.SampleInLap;
 import tk.musial.activityprocessor.SamplesLoader;
 
@@ -15,6 +13,6 @@ public class IndoorCyclingHeartRateSamplesLoader implements SamplesLoader {
   @Override
   public List<SampleInLap> load() {
     throw new RuntimeException(new JsonParseException(null, "some"));
-//    return null;
+    //    return null;
   }
 }
--- activity-indoor-cycling/src/test/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoaderTest.java
+++ activity-indoor-cycling/src/test/java/tk/musial/activityprocessor/indoorcycling/IndoorCyclingHeartRateSamplesLoaderTest.java
@@ -2,24 +2,38 @@ package tk.musial.activityprocessor.indoorcycling;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import java.io.InputStream;
-
-import org.junit.jupiter.api.Test;
-
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.Test;
 
 class IndoorCyclingHeartRateSamplesLoaderTest {
 
   @Test
   void failsToLoadInvalidJson() {
-              var jsonResource =
-                  this.getClass().getResourceAsStream("samples.invalid.json");
+    var jsonResource = this.getClass().getResourceAsStream("samples.invalid.json");
     assertThatExceptionOfType(RuntimeException.class)
         .isThrownBy(
             () -> {
-                var loader = new IndoorCyclingHeartRateSamplesLoader(jsonResource);
-                loader.load();
+              var loader = new IndoorCyclingHeartRateSamplesLoader(jsonResource);
+              loader.load();
             })
-      .withCauseInstanceOf(JsonParseException.class);
+        .withCauseInstanceOf(JsonParseException.class);
+  }
+
+  @Test
+  void failsIfNoSamples() {
+    assertThatExceptionOfType(IllegalStateException.class)
+        .isThrownBy(
+            () -> {
+              var loader = new IndoorCyclingHeartRateSamplesLoader(toInputStream("[]"));
+              loader.load();
+            })
+        .withMessage("Samples are required");
+  }
+
+  private static ByteArrayInputStream toInputStream(String json) throws JsonProcessingException {
+    return new ByteArrayInputStream(new ObjectMapper().writeValueAsBytes(json));
   }
 }

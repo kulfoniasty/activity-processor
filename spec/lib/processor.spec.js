6e267127812c23be2452818347cbb56847b2d821
grzegorz@musial.tk <grzegorz@musial.tk>
2024-09-18 00:00:15 +0200

    test: summary validation while loading

--- spec/lib/processor.spec.js
+++ spec/lib/processor.spec.js
@@ -14,12 +14,23 @@ describe("GIVEN processor", () => {
         processor.loadSummary({ ...summary, activityType: "SNORKLING" });
         done.fail("validation error expected");
       } catch (e) {
+        expect(e).toEqual(new Error("invalid summary"));
         done();
       }
     });
-  });
 
-  it("empty test", () => {
-    processor.loadSummary({}).loadLaps({});
+    it("WHEN invoked without summary parameter THEN error is thrown", (done) => {
+      try {
+        processor.loadSummary();
+        done.fail("validation error expected");
+      } catch (e) {
+        expect(e).toEqual(new Error("invalid summary"));
+        done();
+      }
+    });
+
+    it("WHEN invoked with INDOOR_CYCLING summary it succeeds", () => {
+      processor.loadSummary({ ...summary });
+    });
   });
 });
